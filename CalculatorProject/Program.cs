d6a9bda73a209917fe3395709b92e04cc9e510de
Bashorun407 <83551336+Bashorun407@users.noreply.github.com>
2023-05-02 08:05:23 +0100

    A global result was used instead of individual function results.

--- CalculatorProject/Program.cs
+++ CalculatorProject/Program.cs
@@ -2,21 +2,22 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-double var1 = 0; //This is a global variable representing user's first input
-double var2 = 0; //This is a global variable representing user's second input
+double var1 = 0.0; //This is a global variable representing user's first input
+double var2 = 0.0; //This is a global variable representing user's second input
+double result; //All calculation results will be stored in this variable
 string input = string.Empty; //This string accepts user input
 bool checkDigit = false; //This boolean checks if user enters the correct digit
 bool quitApp = true; //This boolean helps terminate the while loop...and the program
 
 Console.ForegroundColor = ConsoleColor.Green; //Changes the font colour to green
 
-//An infinite loop...which displays users options and runs the entire implementation of subsequent functions
+//A while loop...which displays users options and runs the entire implementation of subsequent functions
 while (quitApp)
 {
     Console.WriteLine("WELCOME...Feel free to do your computations!!!\n");
     Console.WriteLine(" Press 1 for Addition\n Press 2 for Subtraction \n Press 3 for Multiplication \n Press 4 for Division" +
         "\n Press 5 for Exponent \n Press 6 for Square Root \n Press 7 for Sine \n Press 8 for Cosine \n Press 9 for Tangent\n" +
-        "Press 10 to Quit".ToUpper());
+        "Press 10 to Quit\n".ToUpper());
     input = Console.ReadLine();
 
     checkDigit = int.TryParse(input, out int x);
@@ -39,14 +40,14 @@ while (quitApp)
                         var2 = double.Parse(Console.ReadLine());
 
                         //Calls the addition method
-                        double additionResult = addition(var1, var2);
+                        result = addition(var1, var2);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(additionResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
 
                     }
                     catch (Exception ex)
@@ -65,14 +66,14 @@ while (quitApp)
                         var2 = double.Parse(Console.ReadLine());
 
                         //Calls the addition method
-                        double subtractionResult = subtraction(var1, var2);
+                        result = subtraction(var1, var2);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(subtractionResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -90,14 +91,14 @@ while (quitApp)
                         var2 = double.Parse(Console.ReadLine());
 
                         //Calls the addition method
-                        double multiplicationResult = multiplication(var1, var2);
+                        result = multiplication(var1, var2);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(multiplicationResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -115,14 +116,14 @@ while (quitApp)
                         var2 = double.Parse(Console.ReadLine());
 
                         //Calls the addition method
-                        double divisionResult = division(var1, var2);
+                        result = division(var1, var2);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(divisionResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -140,14 +141,14 @@ while (quitApp)
                         var2 = double.Parse(Console.ReadLine());
 
                         //Calls the addition method
-                        double exponentResult = Exponents(var1, (int)var2);
+                        result = Exponents(var1, (int)var2);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(exponentResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -164,14 +165,14 @@ while (quitApp)
                         
 
                         //Calls the addition method
-                        double squareRootResult = SquareRoots(var1);
+                        result = SquareRoots(var1);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(squareRootResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -188,14 +189,14 @@ while (quitApp)
                         
 
                         //Calls the addition method
-                        double sineResult = Sine(var1);
+                        result = Sine(var1);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(sineResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -212,14 +213,14 @@ while (quitApp)
 
 
                         //Calls the addition method
-                        double cosineResult = Cosine(var1);
+                        result = Cosine(var1);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(cosineResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -236,14 +237,14 @@ while (quitApp)
 
 
                         //Calls the addition method
-                        double tangentResult = Tangent(var1);
+                        result = Tangent(var1);
 
                         //Prompts the user for the number of decimal places to approximate answer to
                         Console.WriteLine("Enter the number of decimal places to round result to: ");
                         int decimalPlaces = int.Parse(Console.ReadLine());
 
                         //This function approximates answer to specified number of places
-                        DecimalPrecision(tangentResult, decimalPlaces);
+                        DecimalPrecision(result, decimalPlaces);
                     }
                     catch (Exception ex)
                     {
@@ -260,9 +261,13 @@ while (quitApp)
 
         else
         {
-            Console.WriteLine("You should enter numbers from 1 to 4 only!!");
+            Console.WriteLine("You should enter numbers from 1 to 10 only!!\n");
         }
         
+    }
+    else
+    {
+        Console.WriteLine("Ensure to enter only digits as specified in the drop-down menu.\n");
     }
     //Using a switch to acc
    
@@ -271,7 +276,7 @@ while (quitApp)
 //addition method double
 double addition(double a, double b)
 {
-    Console.WriteLine($"{a} + {b} = {a + b}");
+    Console.WriteLine($"{a} + {b} = {a + b}\n");
     return a + b;
 }
 
@@ -279,7 +284,7 @@ double addition(double a, double b)
 //subtraction methods for double
 double subtraction(double a, double b)
 {
-    Console.WriteLine($"{a} - {b} = {a - b}");
+    Console.WriteLine($"{a} - {b} = {a - b}\n");
     return a - b;
 }
 
@@ -287,7 +292,7 @@ double subtraction(double a, double b)
 //multiplication method for double
 double multiplication(double a, double b)
 {
-    Console.WriteLine($"{a} X {b} = {a * b}");
+    Console.WriteLine($"{a} X {b} = {a * b}\n");
     return a * b;
 }
 
@@ -297,7 +302,7 @@ double division(double a, double b)
 {
     try
     {
-        Console.WriteLine($"{a} / {b} = {a / b}");
+        Console.WriteLine($"{a} / {b} = {a / b}\n");
     }
     catch (Exception e)
     {
@@ -311,7 +316,7 @@ double division(double a, double b)
 double Exponents(double number, int power)
 {
     double answer = Math.Pow(number, power);
-    Console.WriteLine($"Exponent is: {answer}");
+    Console.WriteLine($"{number} raised to power {power} = {answer}\n");
     return answer;
 }
 
@@ -319,7 +324,7 @@ double Exponents(double number, int power)
 double SquareRoots(double number)
 {
     double answer = Math.Sqrt(number);
-    Console.WriteLine($"Exponent is: {answer}");
+    Console.WriteLine($"Square Root of {number} = {answer}\n");
     return answer;
 }
 
@@ -329,7 +334,7 @@ double Sine(double number)
 {
     //Converting each number the user enters to radian...in order to get the correct answer by multiplying number with (3.142/180) = (22.0/1260)
     double answer = Math.Sin(number * (22.0/1260));
-    Console.WriteLine($"Sine is: {answer}");
+    Console.WriteLine($"Sine of {number} = {answer}\n");
     return answer;
 }
 
@@ -338,7 +343,7 @@ double Cosine(double number)
 {
     //Converting each number the user enters to radian...in order to get the correct answer by multiplying number with (3.142/180)
     double answer = Math.Cos(number * (22.0/1260));
-    Console.WriteLine($"Cosine is: {answer}");
+    Console.WriteLine($"Cosine of {number} = {answer}\n");
     return answer;
 }
 
@@ -347,7 +352,7 @@ double Tangent(double number)
 {
     //Converting each number the user enters to radian...in order to get the correct answer by multiplying number with (3.142/180)
     double answer = Math.Tan(number * (22.0/1260)); 
-    Console.WriteLine($"Exponent is: {answer}");
+    Console.WriteLine($"Tangent of {number} = {answer}\n");
     return answer;
 }
 
@@ -359,5 +364,5 @@ void DecimalPrecision(double numberToApproximate, int numberOfDecimalPlaces)
 {
     double approximatedNumber = Math.Round(numberToApproximate, numberOfDecimalPlaces);
 
-    Console.WriteLine($"Answer in {numberOfDecimalPlaces} decimal places is: {approximatedNumber}");
+    Console.WriteLine($"Answer in {numberOfDecimalPlaces} decimal places = {approximatedNumber}\n");
 }
